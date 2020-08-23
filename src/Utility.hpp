69e9f7054f8a7ce90bc88ffb75b74ff5669cf2a7
Kamil Marut <kakkmar@gmail.com>
2020-08-23 22:25:42 +0200

    fix: Typo in function declaration

--- src/Utility.hpp
+++ src/Utility.hpp
@@ -10,9 +10,9 @@ constexpr double infinity = std::numeric_limits<double>::infinity();
 constexpr double pi = 3.14159265358979323846;
 
 // Functions
-inline float degrees_to_radias(double degrees)
+inline float degrees_to_radians(double degrees)
 {
-    return degrees * M_PI / 180;
+    return degrees * pi / 180;
 }
 inline float random_float()
 {
