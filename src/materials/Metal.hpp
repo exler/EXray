4f32d053be2f236dd163f4f23367deddd16b0360
Kamil Marut <kakkmar@gmail.com>
2020-08-24 22:39:06 +0200

    feat: Fuzzy reflection

--- src/materials/Metal.hpp
+++ src/materials/Metal.hpp
@@ -7,12 +7,13 @@
 class Metal : public Material
 {
 public:
-    Metal(const Color3 &a);
+    Metal(const Color3 &a, float f);
 
     virtual bool scatter(const Ray &r_in, const HitRecord &rec, Color3 &attenuation, Ray &scattered) const override;
 
 private:
     Color3 _albedo;
+    float _fuzz;
 };
 
 #endif
